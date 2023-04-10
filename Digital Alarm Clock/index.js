1b8c3e1c23fd88eb34950fb1d783003bf76f1957
Vaibhav <103978586+vaibhav15git@users.noreply.github.com>
2023-04-10 19:04:55 +0530

    all work done

---
+++ Digital Alarm Clock/index.js
@@ -0,0 +1,38 @@
+
+
+
+
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+const dateEl = document.getElementById("date"); // Add an element for displaying date
+
+function updateClock() {
+  let now = new Date(); // Get current date and time
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+  let ampm = "AM";
+  let date = now.toDateString(); // Get date string
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+  dateEl.innerText = date; // Set the date in the date element
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
